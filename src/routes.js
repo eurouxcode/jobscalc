c81e55fad364a3ee174e29e1484c34fdcc3f3c63
Ayres Roux <eu.rouxcode@gmail.com>
2021-11-23 20:03:03 -0300

    Aula 1

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = express.Router()
+
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Jakeliny",
+    avatar: "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4 
+}
+
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile  }))
+
+module.exports = routes;
